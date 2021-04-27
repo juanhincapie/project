ec6b77f3fe6a1c0286c529aca9357a671845c97c
Oriana Sanabria <orianasanabria2@gmail.com>
2021-04-27 01:32:32 -0400

    added javascript smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,20 @@ const img = document.getElementById('image')
 btn.addEventListener('click', () => {
   console.log('u clicked me');
 })
+
+const links = document.querySelectorAll(".navbar ul a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
